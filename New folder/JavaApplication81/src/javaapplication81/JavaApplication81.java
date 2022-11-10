cfdc7f4b12579e2d3bcacb43ae6064e1dcf1c74d
LAPTOP LENOVO <LAPTOP LENOVO@HOAINAM-03>
2022-11-10 11:28:27 +0700

    testing lan thu may quen goi

---
+++ New folder/JavaApplication81/src/javaapplication81/JavaApplication81.java
@@ -0,0 +1,16 @@
+package javaapplication81;
+
+/**
+ *
+ * @author LAPTOP LENOVO
+ */
+public class JavaApplication81 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
